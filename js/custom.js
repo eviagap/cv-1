90adddee9094a093ace94e8c122908f6773c3a30
eviagap <100125644+eviagap@users.noreply.github.com>
2022-03-12 23:49:19 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ Assigned to: ThemeForest
   // Preloader Js
   jQuery(window).on("load", function () {
     jQuery("#status").fadeOut();
-    jQuery("#preloader").delay(200).fadeOut("slow");
+    jQuery("#preloader").delay(50).fadeOut("fast");
   });
 
   // ready function
